56228278df3fb88d5fc9b11a650338c546e07ea8
EloiRigallPages <eloirigallp@gmail.com>
2026-01-14 18:17:52 +0100

    Afegir CarroDeLaCompraRequest

---
+++ Botiga/Botiga/DTO/CarroDeLaCompraRequest.cs
@@ -0,0 +1,18 @@
+﻿using Botiga.Model;
+
+namespace Botiga.DTO
+{
+    public record CarroDeLaCompraRequest(Guid IdCarro, Guid IdProducte, int Quantitat)
+    {
+        public CarroDeLaCompra ToModel(Guid id)
+        {
+            return new CarroDeLaCompra
+            {
+                Id = id,
+                IdCarro = IdCarro,
+                IdProducte = IdProducte,
+                Quantitat = Quantitat
+            };
+        }
+    }
+}
--- Botiga/Botiga/DTO/DTOCarroDeLaCompra.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Botiga.DTO
-{
-    public class DTOCarroDeLaCompra
-    {
-    
-    }
-}
