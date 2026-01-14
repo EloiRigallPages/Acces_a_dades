247fd7f64dd5b6987cad92712222386430627ffb
EloiRigallPages <eloirigallp@gmail.com>
2026-01-14 18:26:50 +0100

    Crar fitxer CarroDeLaCompraValidator

--- Botiga/Botiga/Validators/CarroDeLaCompraValidator.cs
+++ Botiga/Botiga/Validators/CarroDeLaCompraValidator.cs
@@ -1,6 +1,37 @@
-﻿namespace Botiga.Validators
+﻿using Botiga.Common;
+using Botiga.DTO;
+
+namespace Botiga.Validators
 {
-    public class CarroDeLaCompraValidator
+    public static class CarroDeLaCompraValidator
     {
+        public static Result Validate(CarroDeLaCompraRequest req)
+        {
+            if (req.Quantitat <= 0)
+            {
+                return Result.Failure(
+                    "La quantitat ha de ser superior a 0",
+                    "QUANTITAT_INVALIDA"
+                );
+            }
+
+            if (req.IdCarro == Guid.Empty)
+            {
+                return Result.Failure(
+                    "L'IdCarro no pot ser buit",
+                    "ID_CARRO_INVALID"
+                );
+            }
+
+            if (req.IdProducte == Guid.Empty)
+            {
+                return Result.Failure(
+                    "L'IdProducte no pot ser buit",
+                    "ID_PRODUCTE_INVALID"
+                );
+            }
+
+            return Result.Ok();
+        }
     }
 }
