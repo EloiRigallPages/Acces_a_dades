5fdd88f94ad0d55f8c254cf7f347e59160e0e3db
EloiRigallPages <eloirigallp@gmail.com>
2026-01-14 19:40:53 +0100

    carros validator

---
+++ Botiga/Botiga/Validators/CarrosValidator.cs
@@ -0,0 +1,29 @@
+﻿using Botiga.Common;
+using Botiga.DTO;
+
+namespace Botiga.Validators
+{
+    public static class CarrosValidator
+    {
+        public static Result Validate(CarrosRequest req)
+        {
+            if (string.IsNullOrWhiteSpace(req.Nom))
+            {
+                return Result.Failure(
+                    "El nom del carro no pot estar buit",
+                    "NOM_BUIT"
+                );
+            }
+
+            if (req.Nom.Length < 3)
+            {
+                return Result.Failure(
+                    "El nom del carro ha de tenir almenys 3 caràcters",
+                    "NOM_MASSA_CURT"
+                );
+            }
+
+            return Result.Ok();
+        }
+    }
+}
