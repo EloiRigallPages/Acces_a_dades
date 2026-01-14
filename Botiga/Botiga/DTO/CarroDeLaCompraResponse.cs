9d9600335c052c107b2fcdc8ca8903c7a5c367f6
EloiRigallPages <eloirigallp@gmail.com>
2026-01-14 18:22:05 +0100

    Afegir CarroDeLaCompraResponse

---
+++ Botiga/Botiga/DTO/CarroDeLaCompraResponse.cs
@@ -0,0 +1,17 @@
+﻿using Botiga.Model;
+
+namespace Botiga.DTO
+{
+    public record CarroDeLaCompraResponse(Guid Id, Guid IdCarro, Guid IdProducte, int Quantitat)
+    {
+        public static CarroDeLaCompraResponse FromModel(CarroDeLaCompra model)
+        {
+            return new CarroDeLaCompraResponse(
+                model.Id,
+                model.IdCarro,
+                model.IdProducte,
+                model.Quantitat
+            );
+        }
+    }
+}
